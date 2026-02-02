12b1053075396682bfe8406d05b5606cf79cae19
Minh <phu.ngo@outlook.com>
2026-02-02 16:46:27 -0500

    merge from main

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,6 +3,9 @@ import Hero from "./components/Hero";
 import Challenge from "./components/Challenge";
 import Solution from "./components/Solution";
 import CTA from "./components/CTA";
+import TeamSnippet from "./components/TeamSnippet";
+import ContactForm from "./components/ContactForm";
+import Features from "@/components/features";
 import Footer from "./components/Footer";
 
 export default function Page() {
@@ -11,9 +14,13 @@ export default function Page() {
       <Header />
       <main className="flex-1">
         <Hero />
-        <Challenge />
-        <Solution />
-        <CTA />
+        <div className="space-y-0">
+          <Challenge />
+          <Features />
+          <Solution />
+          <CTA />
+
+        </div>
       </main>
       <Footer />
     </div>
